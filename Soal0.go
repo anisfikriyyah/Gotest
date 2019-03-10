fbea42629e20f4210ef9a61423e8198a3c6936f6
anisfikriyyah <anisfikri3@gmail.com>
2019-03-10 02:44:21 -0800

    Finished 0

--- Soal0.go
+++ Soal0.go
@@ -2,9 +2,11 @@
 
 package main
 import "fmt"
+import "strings"
+import "sort"
 
 func main() {
-	fmt.Println(isVowel("a"))
+	vowelConsonan("omama")
 }
 
 func isVowel(s string) bool {
@@ -15,4 +17,30 @@ func isVowel(s string) bool {
 	  }
 	}
 	return false
+}
+
+func vowelConsonan(str string) {
+	cases := str
+	var v string
+	var c string
+	for i:= 0; i < len(cases); i++ {
+		if isVowel(string(cases[i])) {
+			v = v + string(cases[i])
+		} else {
+			c = c + string(cases[i])
+		}
+	}
+	// lakukan split biar bisa diurutkan
+	v_arr := strings.SplitAfter(v, "")
+	sort.Strings(v_arr)
+
+	// looping utk ngilangin data yang sama
+	var ilangSama = []string{string(v_arr[0])}
+	for i:= 1; i < len(v_arr); i++ {
+		if string(v_arr[i]) != string(v_arr[i-1]) {
+			ilangSama = append(ilangSama, string(v_arr[i]))
+		}
+	}
+
+	fmt.Println("huruf mati: ", len(c), ",huruf hidup: ", len(ilangSama) )
 }
